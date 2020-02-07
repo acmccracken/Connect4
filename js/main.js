84dbdf1d4e1919a06a607f1b9899daef0e1bebe5
Alan McCracken <acmccracken@gmail.com>
2020-02-07 08:13:49 -0600

    logic works

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let grid = [[00, 01, 02, 003, 04, 05, 06], [10,11, 12, 13, 14, 15, 16], [20, 21,
 console.log(grid);
 let horz = 0;
 let vert = 0;
-let a = 6, b = 7, c, i, j, k, l, m, n, y, z;
+let a = 6, b = 7, c, i, j, k, l, m, n, o, y, z;
 
 function switchEm(i, j){
     l = i;
@@ -10,8 +10,8 @@ function switchEm(i, j){
     j = l;
 }
 
-for(x = 0; x < 3; x ++){
-    for(i = 0, m = 2, k = 5; i < a; i++){
+for(x = 0; x < 4; x ++){
+    for(i = 0, m = 2, k = 3; i < a; i++){
         horz = 0;
         if(x === 2){
             
@@ -24,6 +24,16 @@ for(x = 0; x < 3; x ++){
                 n = 0;
             }
         }
+        if(x === 3){   
+            z = i, y = j;
+            l = i - 2;
+            o = k;
+            if(l < 0){
+            l = 0;
+            }if(o > 5){
+            o = 5;
+            }
+    }
         for(j = 0; j < b; j++){
             if(x === 1){
                 l = i;
@@ -34,11 +44,15 @@ for(x = 0; x < 3; x ++){
                 z = i, y = j;
                 i = n, j = l;
             }
+            if(x === 3){
+                z = i, y = j;
+                i = o, j = l;
+            }
                 //console.log(l);
                 //console.log(n);
-            if(/*(x < 2) ||*/((x === 2) && (n < 6) && ( l < 7))){
+            if((x < 2) || ((x === 2) && (n < 6) && ( l < 7))  || ((x === 3) && (o >= 0) && ( l < 7))){
                 //console.log(l);
-                //console.log(n);
+                //console.log(j);
                 console.log(grid[i][j]);
                 if(horz >= 0){
                     if(grid[i][j] === 1){
@@ -77,9 +91,16 @@ for(x = 0; x < 3; x ++){
             j = y;
             l++, n++;
             }
+            if(x === 3){
+            //console.log(l);
+            i = z;
+            j = y;
+            l++, o--;
+            }
             
         }
         m--;
+        k++;
         
     }
     if(x === 0){
@@ -88,6 +109,7 @@ for(x = 0; x < 3; x ++){
     }if(x === 1){
         a--;
     }
+
 }
 /*for(j = 0; j < 7; j++){
   vert = 0;
